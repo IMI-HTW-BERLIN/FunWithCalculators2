8f1fbd16c7c317009cd0382fa7a0c1a170518993
David <panagiotopulos.d@gmail.com>
2018-06-06 18:20:49 +0200

    Added more Test cases.

--- test/postfix/PostfixTest.java
+++ test/postfix/PostfixTest.java
@@ -16,9 +16,21 @@ public class PostfixTest {
     public void precedence() throws Exception{
         assertEquals("3.0", pf.evaluate(pf.infixToPostfix("1+1*2")));
         assertEquals("4.0", pf.evaluate(pf.infixToPostfix("(1+1)*2")));
+        assertEquals("13.0", pf.evaluate(pf.infixToPostfix("2+1*2+1*2*1+2*1+1+2*2")));
+
         assertEquals("3.0", pf.evaluate(pf.infixToPostfix("2*1+1")));
         assertEquals("4.0", pf.evaluate(pf.infixToPostfix("2*(1+1)")));
         assertEquals("3.0", pf.evaluate(pf.infixToPostfix("(2*1)+1")));
         assertEquals("3.0", pf.evaluate(pf.infixToPostfix("1+(2*1)")));
+
+        assertEquals("18.0", pf.evaluate(pf.infixToPostfix("2*(2*4+1)")));
+        assertEquals("12.0", pf.evaluate(pf.infixToPostfix("(2*(2+1*4))")));
+        assertEquals("16.0", pf.evaluate(pf.infixToPostfix("(2*2)*4")));
+        assertEquals("16.0", pf.evaluate(pf.infixToPostfix("2*(2*4)")));
+
+        assertEquals("20.0", pf.evaluate(pf.infixToPostfix("2*(2*(4+1))")));
+        assertEquals("18.0", pf.evaluate(pf.infixToPostfix("2*((2*4)+1)")));
+        assertEquals("20.0", pf.evaluate(pf.infixToPostfix("((4+1)*2)*2")));
+        assertEquals("18.0", pf.evaluate(pf.infixToPostfix("(1+(2*4))*2")));
     }
 }
